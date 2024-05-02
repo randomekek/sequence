aee9c2dc75671454616a28de17d0c9892eb165ad
david <202227+randomekek@users.noreply.github.com>
2024-05-02 20:51:49 +1000

    refactor code because i don't want to write new code

--- synthetic.py
+++ synthetic.py
@@ -1,29 +1,77 @@
 # %%
+import dataclasses
+import einops
+import equinox as eqx
 import jax
 import jax.numpy as jnp
 import jax.random as jr
+import optax
+import utils
 
-def recall_task(
-        key,
-        length = 10,
-        fixed_query_vocab = 5,
-        ctx_query_vocab = 5,
-        value_vocab = 10,
-        batches = 10):
-    # vocab = [fixed_query_vocab, ctx_query_vocab, value_vocab]
-    fixed_key, batch_key = jr.split(key)
-    value_vocab_min = fixed_query_vocab + ctx_query_vocab
-    value_vocab_max = value_vocab_min + value_vocab
-    fixed_map = jr.randint(fixed_key, (fixed_query_vocab,), value_vocab_min, value_vocab_max)
-    def make_batch(key):
-        ctx_key, key_key = jr.split(key)
-        ctx_map = jr.randint(ctx_key, (ctx_query_vocab,), value_vocab_min, value_vocab_max)
-        total_map = jnp.concat([fixed_map, ctx_map])
-        keys = jr.randint(key_key, (length,), 0, fixed_query_vocab + ctx_query_vocab)
-        values = jnp.take(total_map, keys)
-        return jnp.ravel(jnp.stack((keys, values)), order='F')
-    return jax.vmap(make_batch)(jr.split(batch_key, batches))
-
-print(recall_task(jr.PRNGKey(1)))
 
 # %%
+def main():
+    ignore_value = -1
+
+    def recall_task(key, length, fixed_query_vocab, ctx_query_vocab, value_vocab, batches):
+        # vocab = [fixed_query_vocab, ctx_query_vocab, value_vocab]
+        fixed_key, batch_key = jr.split(key)
+        value_vocab_min = fixed_query_vocab + ctx_query_vocab
+        value_vocab_max = value_vocab_min + value_vocab
+        fixed_map = jr.randint(fixed_key, (fixed_query_vocab,), value_vocab_min, value_vocab_max)
+
+        def make_batch(key):
+            ctx_key, key_key = jr.split(key)
+            ctx_map = jr.randint(ctx_key, (ctx_query_vocab,), value_vocab_min, value_vocab_max)
+            total_map = jnp.concat([fixed_map, ctx_map])
+            keys = jr.randint(key_key, (length,), 0, fixed_query_vocab + ctx_query_vocab)
+            values = jnp.take(total_map, keys)
+            input = jnp.ravel(jnp.stack([keys, values]), order='F')
+            mask = jnp.arange(input.shape[0]) % 2 == 1
+            output = jnp.where(mask, ignore_value, jnp.concatenate([input[1:], jnp.array([ignore_value])]))
+            return (input, output)
+        return jax.vmap(make_batch)(jr.split(batch_key, batches))
+
+    def get_batch(i):
+        return recall_task(
+            key=jr.PRNGKey(i),
+            length=10,
+            fixed_query_vocab=5,
+            ctx_query_vocab=5,
+            value_vocab=10,
+            batches=2)
+
+    class Model(eqx.Module):
+        pass
+
+    def accuracy_fn(batch):  # ([B, L], [B, L])
+        x, y = batch
+        @eqx.filter_jit
+        def fn(model):
+            prediction = jnp.argmax(jax.vmap(model)(x))  # argmax(softmax(x)) == argmax(x)
+            ignore_mask = y != ignore_value
+            return jnp.mean(ignore_mask * (prediction == y), axis=(0, 1))
+        return fn
+
+    @eqx.filter_value_and_grad
+    def loss_fn(model_dyn, model_const, x, y):  # model, model, [B, L], [B, L]
+        model = eqx.combine(model_dyn, model_const)
+        raw_prediction = jax.vmap(model)(x)  # [B, L, C]
+        prediction = jax.nn.softmax(raw_prediction, axis=2)
+        epsilon = 1e-7
+        prediction = jnp.clip(prediction, epsilon, 1.0 - epsilon)
+        ignore_mask = y != ignore_value
+        return -jnp.mean(ignore_mask * y * jnp.log(prediction))
+
+    model = Model(jr.PRNGKey(0))
+    optimizer = optax.adam(1e-4)  # needs to be lower for more complex models
+
+    base_tree = jax.tree_util.tree_map(lambda _: True, model)
+    def fixed(dln): return [dln.size, dln.theta]
+    def dlns(t): return sum([fixed(t.dln)] + [fixed(l[0]) for l in t.layers], start=[])
+    partition = eqx.tree_at(dlns, base_tree, replace_fn=lambda n: False)
+
+    return utils.optimize(model, optimizer, get_batch, partition, loss_fn, accuracy_fn(get_batch(0)))
+
+
+main()
--- utils.py
+++ utils.py
@@ -5,64 +5,78 @@ import jax
 
 DIR = 'logs/'
 
+
 def run(fn, description):
-  filename = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
-  output = fn()
-  filename = f'{DIR}{filename}.py'
-  log = ''
-  if output and 'log' in output:
-    log = f'\n\n{output["log"]}'
-  with open(filename, 'x') as code:
-    code.write(f'"""\n{description}{log}\n"""\n\n{inspect.getsource(fn)}')
-  with open(f'log.txt', 'a+') as summary:
-    summary.write(f'===\n{filename}\n\n{description}\n\n')
-  return output
+    filename = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
+    output = fn()
+    filename = f'{DIR}{filename}.py'
+    log = ''
+    if output and 'log' in output:
+        log = f'\n\n{output["log"]}'
+    with open(filename, 'x') as code:
+        code.write(f'"""\n{description}{log}\n"""\n\n{inspect.getsource(fn)}')
+    with open(f'log.txt', 'a+') as summary:
+        summary.write(f'===\n{filename}\n\n{description}\n\n')
+    return output
+
 
 def split_shape(key, shape):
-  if isinstance(shape, (tuple, list)):
-    return tuple(split_shape(k, s) for (k, s) in zip(jax.random.split(key, len(shape)), shape))
-  elif shape == 0:
-    return key
-  else:
-    return jax.random.split(key, shape)
+    if isinstance(shape, (tuple, list)):
+        return tuple(split_shape(k, s) for (k, s) in zip(jax.random.split(key, len(shape)), shape))
+    elif shape == 0:
+        return key
+    else:
+        return jax.random.split(key, shape)
+
 
 CONST_LIST = {}
+
+
 def run_const(fn):
-  code = inspect.getsource(fn)
-  if code not in CONST_LIST:
-    print(f'eval: {fn.__name__}')
-    CONST_LIST[code] = fn()
-  return CONST_LIST[code]
+    code = inspect.getsource(fn)
+    if code not in CONST_LIST:
+        print(f'eval: {fn.__name__}')
+        CONST_LIST[code] = fn()
+    return CONST_LIST[code]
 
-def optimize(model, optimizer, get_batch, update, get_train, get_test, opt_state=None):
-  if opt_state is None:
-    opt_state = optimizer.init(model)
-  logs = []
 
-  def log(text, end='\n'):
-    print(text, end=end)
-    logs.append(text)
-    logs.append(end)
+def optimize(model, optimizer, get_batch, partition, loss_fn, accuracy_fn, opt_state=None):
+    if opt_state is None:
+        opt_state = optimizer.init(model)
+    logs = []
 
-  print('starting')
-  t = datetime.datetime.now()
-  displayed = 0
-  try:
-    for i in range(10000):
-      x, y = get_batch(i)
-      loss, model, opt_state = update(model, x, y, opt_state)
-      if i < 1 or (datetime.datetime.now() - t).total_seconds() > 5.0:
-        log(f'{displayed:02d} {i:04d} {loss:.3e} ', end='')
-        log(f'{get_train(model)*100:0.1f}%')
-        t = datetime.datetime.now()
-        displayed += 1
-  except KeyboardInterrupt:
-    print('interrupt')
+    def log(text, end='\n'):
+        print(text, end=end)
+        logs.append(text)
+        logs.append(end)
 
-  log(f'xx {i:04d} train {get_train(model)*100:0.1f}% test {get_test(model)*100:0.1f}% (done)')
+    @eqx.filter_jit
+    def update(model, x, y, opt_state):
+        model_dyn, model_const = eqx.partition(model, partition)
+        loss, grads = loss_fn(model_dyn, model_const, x, y)
+        updates, opt_state = optimizer.update(grads, opt_state)
+        model = eqx.apply_updates(model, updates)
+        return loss, model, opt_state
 
-  return {
-    'model': model,
-    'opt_state': opt_state,
-    'log': ''.join(logs),
-  }
+    print('starting')
+    t = datetime.datetime.now()
+    displayed = 0
+    try:
+        for i in range(10000):
+            x, y = get_batch(i + 1)
+            loss, model, opt_state = update(model, x, y, opt_state)
+            if i < 1 or (datetime.datetime.now() - t).total_seconds() > 5.0:
+                log(f'{displayed:02d} {i:04d} {loss:.3e} ', end='')
+                log(f'{accuracy_fn(model)*100:0.1f}%')
+                t = datetime.datetime.now()
+                displayed += 1
+    except KeyboardInterrupt:
+        print('interrupt')
+
+    log(f'xx {i:04d} accuracy {accuracy_fn(model)*100:0.1f}% (done)')
+
+    return {
+        'model': model,
+        'opt_state': opt_state,
+        'log': ''.join(logs),
+    }
