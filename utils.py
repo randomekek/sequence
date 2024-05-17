091d6b2be52f4fd82e5bf5c90fa3485371da2996
david <202227+randomekek@users.noreply.github.com>
2024-05-17 22:17:10 +1000

    improve utils

--- utils.py
+++ utils.py
@@ -1,19 +1,22 @@
 import datetime
 import equinox as eqx
 import inspect
+import pathlib
 import jax
 
 DIR = 'logs/'
 
 
 def run(fn, description):
+    folder = datetime.datetime.now().strftime('%Y%m')
     filename = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
     outputs = fn()
-    filename = f'{DIR}{filename}.py'
+    filename = f'{DIR}/{folder}/{filename}.py'
     log = ''
     for param in outputs:
-        if type(param) == dict and 'log' in param:
-            log = f'\n\n{param["log"]}'
+        if type(param) == dict and 'logs' in param:
+            log = '\n\n' + param['logs']
+    pathlib.Path(f'{DIR}/{folder}').mkdir(exist_ok=True)
     with open(filename, 'x') as code:
         code.write(f'"""\n{description}{log}\n"""\n\n{inspect.getsource(fn)}')
     with open(f'log.txt', 'a+') as summary:
@@ -42,7 +45,7 @@ def run_const(fn):
     return CONST_LIST[code]
 
 
-def optimize(model, opt_state, update, accuracy_fn):
+def optimize(model, opt_state, update, accuracy_fn, iter_count=2000, seed=0):
     logs = []
 
     def log(text, end='\n'):
@@ -52,9 +55,8 @@ def optimize(model, opt_state, update, accuracy_fn):
     print('starting')
     t = datetime.datetime.now()
     displayed = 0
-    keys = jax.random.split(jax.random.PRNGKey(0), 10000)
     try:
-        for b, key in zip(range(10000), keys):
+        for b, key in zipkey(range(iter_count), jax.random.PRNGKey(seed)):
             loss, model, opt_state = update(key, model, opt_state)
             if b < 1 or (datetime.datetime.now() - t).total_seconds() > 5.0:
                 log(f'{displayed:02d} {b:04d} {loss:.3e} ', end='')
@@ -64,10 +66,14 @@ def optimize(model, opt_state, update, accuracy_fn):
     except KeyboardInterrupt:
         print('interrupt')
 
-    log(f'xx {b:04d} accuracy {accuracy_fn(model, keys[0])*100:0.1f}% (done)')
+    log(f'xx {b:04d} accuracy {accuracy_fn(model, jax.random.PRNGKey(0))*100:0.1f}% (done)')
 
-    return model, opt_state, {'logs': logs}
+    return model, opt_state, {'logs': ''.join(logs)}
 
 
 def param_count(model):
     return sum(x.size for x in jax.tree_util.tree_leaves(model))
+
+
+def zipkey(items, key):
+    return zip(items, jax.random.split(key, len(items)))
